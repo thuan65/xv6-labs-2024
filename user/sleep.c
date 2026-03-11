e041187f6c8f041365c63be7c103372d984b0590
Son-Le-007 <sonphungle1@gmail.com>
2026-03-11 07:00:26 +0700

    feat: Enhance sleep command with input validation and error handling

--- user/sleep.c
+++ user/sleep.c
@@ -1,16 +1,26 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
+int isValidInt(char *s) {
+    if (s[0] == '\0') return 0;
+    for (int i = 0; s[i] != '\0'; i++)
+        if (s[i] < '0' || s[i] > '9')
+            return 0;
+    return 1;
+}
 
 int main(int argc, char *argv[]) {
-
     if (argc != 2) {
-        printf("Need two argument only!");
-        exit(0);
+        fprintf(2, "Need two arguments only\n");
+        exit(1);
     }
-
-    printf("Sleeping for a little while\n");
-    sleep(atoi(argv[1]));
-    printf("woke up\n");
+    if (!isValidInt(argv[1])) {
+        fprintf(2, "Not a valid number\n");
+        exit(1);
+    }
+    int count = atoi(argv[1]);
+    printf("Sleeping for %d ticks\n", count);
+    sleep(count);
+    printf("Woke up\n");
     exit(0);
 }
